e2995124f123376229b8f6a6e473ee13b9794b99
Norris <99494922+bedzon94@users.noreply.github.com>
2023-09-19 10:55:41 -0400

    einstein.py

---
+++ pset0/einstein.py
@@ -0,0 +1,11 @@
+#Takes input
+mass = int(input("m: "))
+
+#Defined figure for speed of light
+sl = int("300000000")
+
+#Formular for c squared
+c = sl ** 2
+
+#Prints results for e=mc^2
+print("E: ", mass * c)
