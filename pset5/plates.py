20a1dac191980a79efc094fc6254359a07c550d9
Norris <99494922+bedzon94@users.noreply.github.com>
2023-09-28 06:06:43 -0400

    plates.py

---
+++ pset5/plates.py
@@ -0,0 +1,22 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if 6 >= len(s) >= 2 and s[0:2].isalpha() and s.isalnum():
+        for letter in s:
+            if letter.isdigit():
+                index = s.index(letter)
+                if s[index:].isdigit() and letter != "0":
+                    return True
+                else:
+                    return False
+        return True
+    return False
+
+if __name__ == "__main__":
+    main()
